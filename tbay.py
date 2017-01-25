138ce180c7f52b9453f02846958a3dd956759c01
jamiegill <jamiegill.03@gmail.com>
2017-01-25 04:18:34 +0000

    updated tbay with assignment 2.2.3 coursework

--- tbay.py
+++ tbay.py
@@ -1,14 +1,12 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship, sessionmaker
 
 engine = create_engine('postgresql://ubuntu:thinkful@localhost:5432/tbay')
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
-
-from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime, Float
 
 class Item(Base):
     __tablename__ = "items"
@@ -18,18 +16,53 @@ class Item(Base):
     description = Column(String)
     start_time = Column(DateTime, default=datetime.utcnow)
     
+    bids = relationship("Bid", backref="bid",)
+    owner_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    
 class User(Base):
     __tablename__ = "user"
     
     id = Column(Integer, primary_key=True)
     username = Column(String, nullable=False)
     password = Column(String, nullable=False)
+    
+    item = relationship("Item", backref="owner")
+    bids = relationship("Bid", backref="bidder")
+
 
 class Bid(Base):
     __tablename__ = "bid"
     
     id = Column(Integer, primary_key=True)
     price = Column(Float, nullable=False)
-
+    
+    user_id = Column(Integer, ForeignKey('user.id'))
+    item_id = Column(Integer, ForeignKey('items.id'))
 
 Base.metadata.create_all(engine)
+Jamie = User(username="jgill", password="testing123")
+Tim = User(username='tmartin', password="testing1234")
+Dan = User(username='dwright', password="testing1235")
+Sammy = User(username='sday', password="testing456")
+session.add_all([Jamie, Tim, Dan])
+session.commit()
+
+clock_radio = Item(name="clock radio",description="alarm buzzing", owner=Jamie)
+#table = Item(name="table", description="four legs")
+session.add(clock_radio)
+session.commit()
+
+tim_bid = Bid(price="30.50", bidder=Tim, bid=clock_radio)
+dan_bid = Bid(price="14.67", bidder=Dan, bid=clock_radio)
+sammy_bid = Bid(price="19.67", bidder=Sammy, bid=clock_radio)
+session.add_all([tim_bid, dan_bid, sammy_bid])
+session.commit()
+
+row = session.query(User.username, Item.name).join(Bid, Item).filter(Item.name == "clock radio").order_by(Bid.price).all()
+bid_winner = row[-1][-2]
+won_prize = row[-1][-1]
+print("{} is the bid winner and won a {}".format(bid_winner,won_prize))
+
+
+
+
